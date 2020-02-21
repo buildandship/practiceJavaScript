1be1aa9f9da4837e2bfa34257f38b70522112efa
Praveen Mishra <nice.prav@gmail.com>
2020-02-21 18:25:03 +0530

    Working version completed

--- js_excercises/ex3/script.js
+++ js_excercises/ex3/script.js
@@ -14,7 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
     let newDiv = document.createElement('div');
     newDiv.id = 'main';
     newDiv.className = 'main';
-    //newDiv.height = '100%';
     newDiv.style.height = '100px';
     newDiv.style.overflow = 'scroll';
     // Iterate map and create parent and child checkboxes
@@ -28,58 +27,86 @@ document.addEventListener("DOMContentLoaded", () => {
         parentCheckbox.addEventListener('click', parentClick);
         parentLabel.for = key;
         parentLabel.innerHTML = key;
-        //console.log('KEY::' + key);
         newDiv.appendChild(parentCheckbox);
         newDiv.appendChild(parentLabel);
+        /*
         map.get(key).forEach(value => {
-            let p = document.createElement('p');
             let childCheckbox = document.createElement('input');
             let childLabel = document.createElement('label');
             childCheckbox.type = 'checkbox';
             childCheckbox.value = value;
             childCheckbox.id = value;
             childCheckbox.name = key;
-            childCheckbox.style.display = 'none';
             childCheckbox.addEventListener('click', childClick);
             childLabel.for = value;
             childLabel.innerHTML = value;
-            childLabel.className = value;
-            childLabel.style.display = 'none';
-            //console.log(value);
-            //  newDiv.appendChild(document.createElement('br'));
+            // newDiv.appendChild(document.createElement('br'));
             // newDiv.appendChild(document.createElement('br'));
             newDiv.appendChild(childCheckbox);
             newDiv.appendChild(childLabel);
 
         });
-        //newDiv.appendChild(document.createElement('br'));
-        //newDiv.appendChild(document.createElement('br'));
+        */
+        // newDiv.appendChild(document.createElement('br'));
+        // newDiv.appendChild(document.createElement('br'));
     });
     container.appendChild(newDiv);
 });
-
-parentClick = (e) => {
-    console.log('Parent Clicked');
-    console.log(e.target.name);
-    console.log('CLICKING ' + this);
-    let childboxes = document.getElementsByName(e.target.id);
-    let childlabels = document.querySelectorAll('.' + e.target.id)
-    for (let i = 0; i < childboxes.length; i++) {
-        childboxes[i].checked = true;
-        childboxes[i].style.display = '';
-        childlabels[i].classList.remove(e.target.id);
-
-    }
-
-}
-
-childClick = (e) => {
-    console.log('Child Clicked');
-}
-
 
 // R3: Now when you click on color checkbox it should display four checkboxes(red, yellow, green, blue) as a child list 
 // and color checkbox should be checked with its child list of checkboxes.
 
 // R4: Again when you click on color checkbox child list should be hidden 
 // and its child checkboxes should get deselected(clear state). Same with movies, drink and bikes.
+parentClick = (e) => {
+    console.log('Parent Clicked');
+    console.log(e.target);
+    console.log(e.target.checked);
+    let key = e.target.id;
+    let divpresent = document.querySelectorAll('#child-' + key);
+    console.log(divpresent.length);
+    if (e.target.checked) {
+        let main = document.querySelector('.main');
+        let newDiv = document.createElement('div');
+        if (divpresent.length === 0) {
+            newDiv.id = 'child-' + key;
+            newDiv.className = 'child-' + key;
+            map.get(key).forEach(value => {
+                let p = document.createElement('p');
+                let childCheckbox = document.createElement('input');
+                let childLabel = document.createElement('label');
+                childCheckbox.type = 'checkbox';
+                childCheckbox.value = value;
+                childCheckbox.id = value;
+                childCheckbox.name = key;
+                childCheckbox.checked = e.target.checked;
+                childCheckbox.addEventListener('click', childClick);
+                childLabel.for = value;
+                childLabel.innerHTML = value;
+                newDiv.appendChild(document.createElement('br'));
+                newDiv.appendChild(document.createElement('br'));
+                newDiv.appendChild(childCheckbox);
+                newDiv.appendChild(childLabel);
+            });
+
+            main.appendChild(newDiv);
+        }
+        document.getElementById('child-' + key).style.display = 'block';
+        document.getElementById('child-' + key).scrollIntoView();
+    } else if (!e.target.checked) {
+        //document.getElementById('child-' + key).remove();
+        document.getElementById('child-' + key).style.display = 'none';
+        let childboxes = document.getElementsByName(e.target.id);
+        childboxes.forEach(b => {
+            b.checked = false;
+        });
+
+    }
+}
+
+childClick = (e) => {
+    console.log('Child Clicked');
+    console.log(e.target);
+    console.log(e.target.checked);
+}
+
